11d2d779b485b866fdbece248dab7a956108360b
ChenYuxuan_NPU <chenyuxuan_npu@mail.nwpu.edu.cn>
2024-10-24 08:43:14 +0800

    补充代码注释

--- calculation/tool/func.py
+++ calculation/tool/func.py
@@ -2,8 +2,14 @@ import json
 from pathlib import Path
 
 
-def load_json_data(folder: str, file_name: str) -> dict:
-    # 读取现有json文件
+def load_json_data(folder: str, file_name: str) -> dict | None:
+    """
+    根据文件夹名和json文件名读取json文件中的数据
+    :param folder: json_file下的文件夹名
+    :param file_name: 文件夹内的json文件名（不带json后缀）
+    :return: dict型数据
+    """
+
     with open(fr"{Path(__file__).resolve().parent.parent.parent}\json_file\{folder}\{file_name}.json",
               "r", encoding="UTF-8") as f:
         json_data = json.load(f)
@@ -12,6 +18,13 @@ def load_json_data(folder: str, file_name: str) -> dict:
 
 
 def save_json_data(json_data: dict, folder: str, file_name: str) -> None:
+    """
+    将dict文件覆盖至json_file下某个文件夹的某个json文件中
+    :param json_data: 需要保存的dict数据
+    :param folder: json_file下的文件夹名
+    :param file_name: 文件夹内的json文件名（不带json后缀）
+    :return:
+    """
     with open(fr"{Path(__file__).resolve().parent.parent.parent}\json_file\{folder}\{file_name}.json",
               "w", encoding="UTF-8") as f:
         # 将生成的数据保存至json文件中
