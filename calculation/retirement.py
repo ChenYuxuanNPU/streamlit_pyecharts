6ae4f03e2a40fb20f547522812e743b7ddb320db
ChenYuxuan_NPU <chenyuxuan_npu@mail.nwpu.edu.cn>
2024-10-24 09:18:41 +0800

    用身份证计算年龄

--- calculation/retirement.py
+++ calculation/retirement.py
@@ -1,14 +1,31 @@
 from datetime import datetime
-
 from dateutil.relativedelta import relativedelta
 
-# 定义两个日期
-date1 = datetime(2019, 4, 2)
-date2 = datetime(2023, 4, 3)
 
-# 计算两个日期之间的间隔
-delta = abs(relativedelta(date1, date2).years)
+def get_age_from_citizen_id(citizen_id: str) -> int:
+    """
+    通过身份证号计算当前年龄
+    :param citizen_id: 身份证号
+    :return: 年龄
+    """
 
-# 输出间隔的天数
-print(f"{delta}")
+    if len(citizen_id) != 18:
+        return 0
 
+    return abs(
+        relativedelta(
+            datetime(
+                year=int(citizen_id[6:10]),
+                month=int(citizen_id[10:12]),
+                day=int(citizen_id[12:14])
+            ),
+            datetime.today()
+        ).years
+    )
+
+
+if __name__ == '__main__':
+
+    str1 = "440000200102220000"
+
+    print(get_age_from_citizen_id(citizen_id=str1))
