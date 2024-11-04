b5f7677db4e8ea3801fe7f296eb3d0e7dac9d910
ChenYuxuan_NPU <chenyuxuan_npu@mail.nwpu.edu.cn>
2024-11-04 11:16:36 +0800

    1.完善取值范围，对时间顺序加入了要求（身份证晚于查询日期返回0） 2.加入了截止日期，用于计算身份证截止某一年月日（通常是9月1日）的年龄

--- calculation/retirement.py
+++ calculation/retirement.py
@@ -1,14 +1,18 @@
 from datetime import datetime
+
 from dateutil.relativedelta import relativedelta
 
 from teacher_data_processing.tool import func as tch_proc_func
 
 
-def get_age_from_citizen_id(citizen_id: str) -> int:
+def get_age_from_citizen_id(citizen_id: str, year: int = None, month: int = 9, day: int = 1) -> int:
     """
-    通过身份证号计算当前年龄\n
+    通过身份证号计算当前年龄或截止某一年某一月某一日（默认某一年的9月1日）\n
     get_age_from_citizen_id(citizen_id = "440105200102220000") -> 23
     :param citizen_id: 身份证号
+    :param year: 截止年份
+    :param month: 截止月份
+    :param day: 截止日期
     :return: 年龄,两位数int
     """
 
@@ -16,16 +20,35 @@ def get_age_from_citizen_id(citizen_id: str) -> int:
         return 0
 
     try:
-        return abs(
-            relativedelta(
-                datetime(
-                    year=int(citizen_id[6:10]),
-                    month=int(citizen_id[10:12]),
-                    day=int(citizen_id[12:14])
-                ),
-                datetime.today()
-            ).years
-        )
+        if year is None:
+            return max(
+                relativedelta(
+                    dt1=datetime.today(),
+                    dt2=datetime(
+                        year=int(citizen_id[6:10]),
+                        month=int(citizen_id[10:12]),
+                        day=int(citizen_id[12:14])
+                    )
+                ).years,
+                0
+            )
+
+        elif 2000 <= year <= 3000:
+            return max(
+                relativedelta(
+                    dt1=datetime(year=year, month=month, day=day),
+                    dt2=datetime(
+                        year=int(citizen_id[6:10]),
+                        month=int(citizen_id[10:12]),
+                        day=int(citizen_id[12:14])
+                    )
+
+                ).years,
+                0
+            )
+
+        else:
+            return -1
 
     except Exception as e:
         tch_proc_func.print_color_text(text=f"{e}:{citizen_id}")
@@ -33,7 +56,6 @@ def get_age_from_citizen_id(citizen_id: str) -> int:
 
 
 if __name__ == '__main__':
-
     str1 = "440000200102220000"
 
-    print(get_age_from_citizen_id(citizen_id=str1))
+    print(get_age_from_citizen_id(citizen_id=str1, year=2002))
