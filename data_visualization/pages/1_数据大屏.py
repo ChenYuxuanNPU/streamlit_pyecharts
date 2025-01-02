b424e0075a53d5c1bda1fcbbbde4aa26eaca07d4
ChenYuxuan_NPU <chenyuxuan_npu@mail.nwpu.edu.cn>
2025-01-02 08:34:28 +0800

    1.合并不同页面get_year_list() 2.将单选框更新为多选框，更新展示逻辑

--- data_visualization/pages/1_数据大屏.py
+++ data_visualization/pages/1_数据大屏.py
@@ -16,19 +16,6 @@ session_state_reset(page=1)
 # 设置全局属性
 set_page_configuration(title="教育数字大屏", icon=":sparkler:")
 
-
-def get_year_list() -> list:
-    """
-    获取学校信息年份列表并按照年份逆序排序（由后到前）
-    :return:
-    """
-
-    return sorted(
-        load_json_data(folder="database", file_name="database_basic_info")["list_for_update_school_info"],
-        reverse=True
-    )
-
-
 st.markdown(
     body="<h1 style='text-align: center;'>学校信息总览</h1>",
     unsafe_allow_html=True
@@ -36,35 +23,26 @@ st.markdown(
 
 st.divider()
 
-left, right = st.columns(spec=2)
+year = st.multiselect(
+    label="请选择需要查询的年份",
+    placeholder="必选项",
+    options=get_year_list(kind="school_info"),
+    default=[get_year_list(kind="school_info")[0]],
+)
 
-with left:
-    year_0 = st.selectbox(
-        label="请选择需要查询的年份",
-        options=get_year_list(),
-        index=0,
-    )
+if len(list(year)) == 1:
+    show_pie_chart_info(year=year[0])
 
-with right:
-    year_1 = st.selectbox(
-        label="请选择需要比较的年份",
-        options=get_year_list(),
-        index=None,
-        placeholder="可选项"
-    )
+    show_summarized_info(year=year[0])
 
-show_pie_chart_info(year=year_0)
+    # 某学段展示
 
-show_summarized_info(year=year_0)
+    # 展示信息时
+    if st.session_state.page1_show_detail:
 
-# 某学段展示
+        show_period_detail_info(year=year[0])
 
-# 展示信息时
-if st.session_state.page1_show_detail:
-
-    show_period_detail_info(year=year_0)
-
-# 不展示信息时
-else:
-    # 放一个展开信息的按钮
-    show_detail_button()
+    # 不展示信息时
+    else:
+        # 放一个展开信息的按钮
+        show_detail_button()
