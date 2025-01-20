89ac346480c1b4fdaa206dda73fd0361311c24e5
ChenYuxuan_NPU <chenyuxuan_npu@mail.nwpu.edu.cn>
2025-01-20 09:35:36 +0800

    更新学校数据的json文件生成

--- school_data_processing/make_json/update.py
+++ school_data_processing/make_json/update.py
@@ -1,5 +1,5 @@
 from school_data_processing.make_json.summary import school_data_sum
-from school_data_processing.tool import func as sch_proc_func
+from school_data_processing.tool.func import *
 
 
 def get_year_list() -> dict:
@@ -7,7 +7,7 @@ def get_year_list() -> dict:
     获取数据库中包含的学校信息的年份列表
     :return:
     """
-    return sch_proc_func.load_json_data(folder="database", file_name="database_basic_info")["list_for_update_school_info"]
+    return load_json_data(folder="database", file_name="database_basic_info")["list_for_update_school_info"]
 
 
 if __name__ == '__main__':
--- school_data_processing/read_database/update_database_data.py
+++ school_data_processing/read_database/update_database_data.py
@@ -10,7 +10,7 @@ def get_school_data_sum(json_data: dict, year: str, ) -> dict:
     """
 
     table_name = load_json_data(folder="database", file_name="database_basic_info")["table_name_dict"][
-        f"{year}年学校情况一览表"]
+        f"{year}年学校信息总览"]
 
     temp = []
     result = []
@@ -30,7 +30,7 @@ def get_school_data_sum(json_data: dict, year: str, ) -> dict:
         conn.commit()
 
     for item in result:
-        temp.append(item[1])
+        temp.append(item[1])  # temp存放的是数据库中表的字段列表
 
     # 然后提取数据
     try:
@@ -45,11 +45,11 @@ def get_school_data_sum(json_data: dict, year: str, ) -> dict:
 
     for item in result:
 
-        for i in range(1, len(temp)):
-            json_data = dict_assignment(route=f"{year}/{item[0]}/{temp[i]}", value=int(item[i]), json_data=json_data)
+        for i in range(2, len(temp)):
+            json_data = dict_assignment(route=f"{item[0]}/{item[1]}/{temp[i]}", value=int(item[i]), json_data=json_data)
 
-        if item[0] != "合计":
-            period_list.append(item[0])
+        if item[1] != "合计":
+            period_list.append(item[1])
 
     # 生成学段列表
     json_data = dict_assignment(route=f"学段列表", value=period_list, json_data=json_data)
