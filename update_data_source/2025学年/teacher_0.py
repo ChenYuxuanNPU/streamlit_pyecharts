6adeae427447119ee5a6199a8e28886287d6e8e9
ChenYuxuan_NPU <chenyuxuan_npu@mail.nwpu.edu.cn>
2025-10-23 16:35:59 +0800

    在编教师数据更新

--- update_data_source/2025学年/teacher_0.py
+++ update_data_source/2025学年/teacher_0.py
@@ -8,22 +8,24 @@ from openpyxl.styles import PatternFill
 from openpyxl.styles import numbers
 from openpyxl.utils import get_column_letter
 from openpyxl.worksheet.datavalidation import DataValidation
+from openpyxl.worksheet.protection import SheetProtection
 
-from func import read_xlsx_to_list
+from func import read_xlsx_to_list, generate_subsets
+
+# 下次用的时候，记得限制本科及以上不能不选或填学校
 
 title_0 = [
     "单位全称（按照单位公章）", "学校类型", "统一社会信用代码", "姓名", "身份证号码（文本格式）", "性别",
     "民族", "籍贯（精确到市）", "婚姻状况", "政治面貌", "入党（团）时间", "参加工作前学历",
     "参加工作前学位", "参加工作前毕业院校（普通高校）", "参加工作前毕业院校（境外高校或其他院校）",
-    "参加工作前毕业时间", "参加工作前所学专业全称", "最高学历", "最高学位", "最高学历毕业院校（普通高校）",
-    "最高学历毕业院校（境外高校或其他院校）",
-    "最高学历毕业时间", "最高学历所学专业全称",
+    "参加工作前毕业时间", "参加工作前所学专业全称", "最高学历", "最高学历对应学位", "最高学历毕业院校（普通高校）",
+    "最高学历毕业院校（境外高校或其他院校）", "最高学历毕业时间", "最高学历所学专业全称",
     "参加工作时间", "首次进入白云教育时间", "入白云区事业编时间", "进入现单位任教时间",
-    "行政职务", "任现行政职务级别的时间", "任教年级",
-    "主教学科（只能选一门）", "兼教学科(可以填多门，逗号隔开)", "现持有最高职称", "职称证专业",
-    "职称证发证时间（xxxx年xx月，文本型）", "最高职称证书认定或评审通过时间", "现聘专业技术职称",
-    "现聘职级（高级教师/一二级教师，不是专业技术级别）首次聘用时间（xxxx年xx月，文本型）",
-    "专业技术岗位", "专业技术岗位聘用时间",
+    "行政职务", "任现行政职务级别的时间", "任教年级（从低到高填写，按照列表要求）",
+    "主教学科（只能选一门）", "兼教学科(可以填多门，逗号隔开)", "现持有最高职称", "职称证专业（填学段学科）",
+    "职称证发证时间", "最高职称证书认定或评审通过时间", "现聘专业技术职称",
+    "现聘职级（高级教师/一二级教师，不是专业技术级别）首次聘用时间",
+    "现聘专业技术岗位", "现聘专业技术岗位首次聘用时间",
     "普通话层次", "教师资格层次", "教师资格学科", "最高骨干教师级别", "四名工作室主持人名称（最高级别）",
     "四名工作室主持人确定时间（最高级别）", "曾获市级及以上综合荣誉（可填多个，逗号隔开）",
     "曾获市级以上人才项目称号（可填多个，逗号隔开）", "支教单位全称（只填最近一次,限白云区教育局派出）",
@@ -31,25 +33,35 @@ title_0 = [
     "交流单位全称（只填2015年以后且最近一次交流的情况，不包括跟岗和集中办公）", "交流开始时间", "交流结束时间",
     "现常住地址", "手机号码", "教育网短号", "紧急联系人", "与联系人关系", "紧急联系人手机号码",
     "区域", "主要任教学段", "2025年9月编制所在学段", "2025年9月在岗状态", "是否音体美专任教师", "党内职务级别",
-    "校内其他职务"
-    , "备注"
+    "校内其他职务", "备注", "非学历教育最高学位（如同等学力申硕）", "非学历教育最高学位毕业院校",
+    "非学历教育最高学位毕业院校（境外高校或其他院校）"
 ]
 lst = {
-    "list_b": ["十二年一贯制", "完全中学", "高中", "中职", "九年一贯制", "初中", "小学", "幼儿园", "教学支撑单位"],
+    "list_b": ["十二年一贯制", "完全中学", "高中", "中职", "九年一贯制", "初中", "小学", "幼儿园", "特殊教育学校",
+               "教学支撑单位"],
     "list_f": ["男", "女"],
+    "list_g": ["汉族", "蒙古族", "回族", "藏族", "维吾尔族", "苗族", "彝族", "壮族", "布依族", "朝鲜族", "满族", "侗族",
+               "瑶族", "白族", "土家族", "哈尼族", "哈萨克族", "傣族", "黎族", "傈僳族", "佤族", "畲族", "高山族",
+               "拉祜族", "水族", "东乡族", "纳西族", "景颇族", "柯尔克孜族", "土族", "达斡尔族", "仫佬族", "羌族",
+               "布朗族", "撒拉族", "毛南族", "仡佬族", "锡伯族", "阿昌族", "普米族", "塔吉克族", "怒族", "乌孜别克族",
+               "俄罗斯族", "鄂温克族", "德昂族", "保安族", "裕固族", "京族", "塔塔尔族", "独龙族", "鄂伦春族", "赫哲族",
+               "门巴族", "珞巴族", "基诺族"],
     "list_i": ["未婚", "已婚", "离异", "丧偶", "复婚", "再婚"],
     "list_j": ["群众", "中共党员", "共青团员", "民进会员", "民盟盟员", "致公党员", "中共预备党员", "民革会员",
                "九三学社", "农工党员", "无党派人士", "其他"],
     "list_l": ["博士研究生", "硕士研究生", "本科", "专科", "中专（非师范）", "中师", "高中", "初中"],
     "list_m": ["博士学位", "硕士学位", "学士学位", "无"],
-    "list_n": ["其他"] + [item[0] for item in read_xlsx_to_list(
+    "list_n": ["其他", "无"] + [item[0] for item in read_xlsx_to_list(
         r"C:\Users\1012986131\Desktop\python\streamlit_pyecharts\参考资料\2025年6月院校名单.xlsx")[1:]],
     "list_r": ["博士研究生", "硕士研究生", "本科", "专科", "中专（非师范）", "中师", "高中", "初中"],
     "list_s": ["博士学位", "硕士学位", "学士学位", "无"],
-    "list_t": ["其他"] + [item[0] for item in read_xlsx_to_list(
+    "list_t": ["其他", "无"] + [item[0] for item in read_xlsx_to_list(
         r"C:\Users\1012986131\Desktop\python\streamlit_pyecharts\参考资料\2025年6月院校名单.xlsx")[1:]],
-    "list_ab": ["党组织书记", "党组织书记兼校长", "正校级", "副校级", "中层正职", "中层副职", "少先队大队辅导员",
+    "list_ab": ["书记（正校级）", "副书记（副校级）", "正校级", "副校级", "中层正职", "中层副职", "少先队大队辅导员",
                 "少先队副大队辅导员", "工会主席", "工会副主席", "团委书记", "团委副书记", "无"],
+    "list_ad": generate_subsets(
+        ["幼儿园", "一年级", "二年级", "三年级", "四年级", "五年级", "六年级", "七年级", "八年级",
+         "九年级", "高一年级", "高二年级", "高三年级", "中职一年级", "中职二年级", "中职三年级"]) + ["无"],
     "list_ae": ["语文", "数学", "英语", "思想政治", "历史", "地理", "物理", "化学", "生物", "体育", "音乐", "美术",
                 "书法", "舞蹈", "科学", "信息技术", "通用技术", "劳动", "综合实践", "心理健康", "人工智能", "汽修",
                 "烹饪", "幼儿教育", "电子商务", "特殊教育", "校本课程", "其他", "无"],
@@ -61,8 +73,7 @@ lst = {
                 "专业技术七级（暂聘）", "专业技术八级", "专业技术八级（暂聘）", "专业技术九级", "专业技术九级（暂聘）",
                 "专业技术十级", "专业技术十级（暂聘）", "专业技术十一级", "专业技术十一级（暂聘）", "专业技术十二级",
                 "专业技术十二级（暂聘）", "专业技术十三级", "专业技术十三级（暂聘）", "试用期（未定级）", "管理七级",
-                "管理八级",
-                "管理九级", "普通工", "中级工", "高级工", ],
+                "管理八级", "管理九级", "普通工", "中级工", "高级工", ],
     "list_ao": ["一级甲等", "一级乙等", "二级甲等", "二级乙等", "三级甲等", "三级乙等", "无"],
     "list_ap": ["幼儿园", "小学", "初级中学", "高级中学", "中职专业课", "中职实习指导教师", "高等学校", "无"],
     "list_ar": ["广东省骨干教师", "广州市骨干教师", "白云区骨干教师", "其他", "无"],
@@ -74,6 +85,9 @@ lst = {
                 "长期事假（7天以上）", "产假（不在岗）", "公假（丧假、婚假等）", "长期病休（不在岗）", "其他"],
     "list_bn": ["是", "否"],
     "list_bo": ["党总支（党委）书记", "党总支（党委）副书记", "无"],
+    "list_br": ["博士学位", "硕士学位", "学士学位", "无"],
+    "list_bs": ["其他", "无"] + [item[0] for item in read_xlsx_to_list(
+        r"C:\Users\1012986131\Desktop\python\streamlit_pyecharts\参考资料\2025年6月院校名单.xlsx")[1:]],
 }
 
 
@@ -153,6 +167,30 @@ def ensure_folders_exist_or_clear(kind: str, area_name1="", school_name1=""):
 
 
 def output_excel_0(title: list, data: list, file_name: str, area_name: str):
+    def add_sheet_limit(sheet):
+        protection = SheetProtection(
+            sheet=True,  # 启用保护
+            selectLockedCells=False,  # 禁止选择锁定单元格
+            selectUnlockedCells=False,  # 禁止选择未锁定单元格
+            formatCells=False,  # 禁止格式化单元格
+            formatColumns=False,  # 禁止格式化列
+            formatRows=False,  # 禁止格式化行
+            insertColumns=False,  # 禁止插入列
+            insertRows=False,  # 禁止插入行
+            insertHyperlinks=False,  # 禁止插入超链接
+            deleteColumns=False,  # 禁止删除列
+            deleteRows=False,  # 禁止删除行
+            sort=False,  # 禁止排序
+            autoFilter=False,  # 禁止自动筛选
+            pivotTables=False,  # 禁止数据透视表
+            objects=False,  # 禁止操作对象
+            scenarios=False  # 禁止方案
+        )
+
+        protection.password = 'byjyy'
+
+        sheet.protection = protection
+
     def restrict(options: list, chara):
 
         for i, item in enumerate(options, 1):
@@ -286,7 +324,7 @@ def output_excel_0(title: list, data: list, file_name: str, area_name: str):
 
                 formula += f",COUNTBLANK(数据表!A{column_name}:A{column_name})"
 
-            for i in range(1, 18):  # BS是Excel中的第702列
+            for i in range(1, 20):  # BS是Excel中的第702列
                 # 生成列名（AA-AZ）
                 column_name = chr(ord(column_letter) + i - 1)
 
@@ -310,8 +348,9 @@ def output_excel_0(title: list, data: list, file_name: str, area_name: str):
 
         # 然后判断下拉列表的内容是否被更改
         for i, cell in enumerate(
-                ["B", "F", "I", "J", "L", "M", "N", "R", "S", "T", "AB", "AE", "AG", "AK", "AM", "AO", "AP", "AR", "AX",
-                 "BJ", "BK", "BL", "BM", "BN", "BO"]):
+                ["B", "F", "G", "I", "J", "L", "M", "N", "R", "S", "T", "AB", "AD", "AE", "AG", "AK", "AM", "AO", "AP",
+                 "AR", "AX", "BJ", "BK", "BL", "BM", "BN", "BO", "BR", "BS"]):
+            # [item.split("_")[-1].upper() for item in lst.keys()]
             ws_check[f'E{3 * i + 1}'] = f'{cell}列下拉列表'
             ws_check[f'E{3 * i + 2}'] = gen_list_check_sentence(location=f"{cell}")
             ws_check[f'E{3 * i + 2}'].font = font
@@ -384,26 +423,26 @@ def output_excel_0(title: list, data: list, file_name: str, area_name: str):
         ws_check[f'K11'].font = font
         cell_list.append(f'K11')
 
-        # RS列最高学历学位对照
-        ws_check[f'K13'] = "R/S列最高学历学位-无学位学历"
+        # RS列最高学历对应学位对照
+        ws_check[f'K13'] = "R/S列最高学历对应学位-无学位学历"
         ws_check[
             f'K14'] = '=IF(SUMPRODUCT(ISNUMBER(MATCH(数据表!R:R,{"初中","高中","中师","中专（非师范）","专科"},0))*ISNUMBER(MATCH(数据表!S:S,{"无"},0)))=SUM(COUNTIF(数据表!R:R,{"初中","高中","中师","中专（非师范）","专科"})),1,0)'
         ws_check[f'K14'].font = font
         cell_list.append(f'K14')
 
-        ws_check[f'K16'] = "R/S列最高学历学位-本科"
+        ws_check[f'K16'] = "R/S列最高学历对应学位-本科"
         ws_check[
             f'K17'] = '=IF(SUMPRODUCT(ISNUMBER(MATCH(数据表!R:R,{"本科"},0))*ISNUMBER(MATCH(数据表!S:S,{"学士学位","无"},0)))=SUM(COUNTIF(数据表!R:R,{"本科"})),1,0)'
         ws_check[f'K17'].font = font
         cell_list.append(f'K17')
 
-        ws_check[f'K19'] = "R/S列最高学历学位-硕士研究生"
+        ws_check[f'K19'] = "R/S列最高学历对应学位-硕士研究生"
         ws_check[
             f'K20'] = '=IF(SUMPRODUCT(ISNUMBER(MATCH(数据表!R:R,{"硕士研究生"},0))*ISNUMBER(MATCH(数据表!S:S,{"硕士学位","无"},0)))=SUM(COUNTIF(数据表!R:R,{"硕士研究生"})),1,0)'
         ws_check[f'K20'].font = font
         cell_list.append(f'K20')
 
-        ws_check[f'K22'] = "R/S列最高学历学位-博士研究生"
+        ws_check[f'K22'] = "R/S列最高学历对应学位-博士研究生"
         ws_check[
             f'K23'] = '=IF(SUMPRODUCT(ISNUMBER(MATCH(数据表!R:R,{"博士研究生"},0))*ISNUMBER(MATCH(数据表!S:S,{"博士学位","无"},0)))=SUM(COUNTIF(数据表!R:R,{"博士研究生"})),1,0)'
         ws_check[f'K23'].font = font
@@ -452,6 +491,9 @@ def output_excel_0(title: list, data: list, file_name: str, area_name: str):
     set_col_width(ws=ws_check)
     set_col_width(ws=ws_list)
 
+    add_sheet_limit(sheet=ws_check)
+    add_sheet_limit(sheet=ws_list)
+
     set_all_cell_borders(ws)
 
     # 将整个工作表的默认数字格式设置为文本
@@ -480,11 +522,12 @@ if __name__ == '__main__':
     output = {item: {} for item in ["直管", "新市", "永平", "石井", "江高", "人和", "太和", "钟落潭"]}
 
     for item in data:
-        item[-1] = "无"
-        if item[0] not in output[item[-8]].keys():
-            output[item[-8]][item[0]] = []
+        item[-1] = ""
+        item.extend(["", "", ""])
+        if item[0] not in output[item[-11]].keys():
+            output[item[-11]][item[0]] = []
 
-        output[item[-8]][item[0]].append(item)
+        output[item[-11]][item[0]].append(item)
 
     with open(fr"test.json", "w", encoding="UTF-8") as f:
         # 将生成的数据保存至json文件中
